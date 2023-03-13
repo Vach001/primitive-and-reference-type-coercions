3b656b146f3604d7330f5b30fc5fa95cb8c2a2e0
Vache Apinyan <apinyan.v@gmail.com>
2023-03-13 21:01:27 +0400

    feat(exercises): add exercises.js file and read object with field name.

---
+++ exercises.js
@@ -0,0 +1,17 @@
+// object with field name
+
+const object = {
+    _name: [],      // [name, length][]
+    
+    set name(value) {
+        const names = value.split(', ');
+        names.map((item) => object._name.push([item, item.length]));
+  },
+    get name() {
+        return object._name;
+    }, 
+};
+
+object.name = 'Artak, Artavazd, Samvel';
+
+console.log(object.name) //c[['Artak',5],['Artavazd', 8],['Samvel, 6]];

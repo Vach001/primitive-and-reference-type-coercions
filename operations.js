7333af2ac87462fc5d7fa6e7e5663a65c8922c2d
Vache Apinyan <apinyan.v@gmail.com>
2023-03-13 04:07:59 +0400

    feat(operations): add instanceof operator.

--- operations.js
+++ operations.js
@@ -106,4 +106,15 @@ d[d.hasOwnProperty('valueOf') ? 'valueOf' : 'toString'] () // 22
 e[e.hasOwnProperty('valueOf') ? 'valueOf' : 'toString'] () // '[object Object]'
 f[f.hasOwnProperty('valueOf') ? 'valueOf' : 'toString'] () // 5
 
+// instanceof operator
+
+// null instanceof Object                 // false
+// true instanceof Boolean                // false
+// false instanceof Boolean               // false
+// 1 instanceof Number                    // false
+// NaN instanceof Number                  // false
+// 'hello' instanceof String              // false
+// (function () { }) instanceof Function  // true
+
+
 
